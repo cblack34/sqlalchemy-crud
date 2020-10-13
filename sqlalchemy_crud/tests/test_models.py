9d4d918e9aed830b348ae5ddafdc587d146d2f48
Clayton Black <cblack@fanatics.com>
2020-10-12 23:04:40 -0400

    Add test cases

---
+++ sqlalchemy_crud/tests/test_models.py
@@ -0,0 +1,41 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, func, Table
+from sqlalchemy.orm import relationship
+
+from sqlalchemy_crud.tests.test_database import Base
+
+parents_to_children = Table(
+    "parents_to_children",
+    Base.metadata,
+    Column("parent_id", Integer, ForeignKey("parent_1.id")),
+    Column("child_id", Integer, ForeignKey("child.id")),
+)
+
+
+class Parent(Base):
+    __tablename__ = "parent_1"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), nullable=False, unique=True)
+    created = Column(DateTime(timezone=True), server_default=func.now())
+    updated = Column(
+        DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
+    )
+
+    children = relationship(
+        "Child", secondary="parents_to_children", back_populates="parents"
+    )
+
+
+class Child(Base):
+    __tablename__ = "child"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), nullable=False, unique=True)
+    created = Column(DateTime(timezone=True), server_default=func.now())
+    updated = Column(
+        DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
+    )
+
+    parents = relationship(
+        "Parent", secondary="parents_to_children", back_populates="children"
+    )
--- sqlalchemy_crud/tests/test_sqlalchemy_crud.py
+++ sqlalchemy_crud/tests/test_sqlalchemy_crud.py
@@ -1,5 +1,65 @@
-from sqlalchemy_crud import __version__
+import time
+
+import pytest
+
+from sqlalchemy_crud.tests.test_database import setup_database
+from sqlalchemy_crud import crud
+from sqlalchemy_crud.tests import test_models
+
+parent_1 = {"name": "John"}
+parent_2 = {"name": "Paul"}
+parent_2 = {"name": "Mary"}
+
+bad_parent_1 = {"invalid_field": "That's not what your mom said."}
+
+child = {"name": "Mike"}
 
 
-def test_version():
-    assert __version__ == "v0.1.0-beta"
+def test_create_get_model(setup_database):
+    db = setup_database
+
+    created_parent: test_models.Parent = crud.create_model(
+        db, test_models.Parent, parent_1
+    )
+    db_parent: test_models.Parent = crud.get_model(
+        db, test_models.Parent, created_parent.id
+    )
+
+    # time.sleep(100)
+
+    assert parent_1["name"] == db_parent.name
+
+
+def test_create_raise_exception_on_invalid_attribute(setup_database):
+    db = setup_database
+
+    with pytest.raises(TypeError) as excinfo:
+        crud.create_model(db, test_models.Parent, bad_parent_1)
+
+
+def test_update_model(setup_database):
+    db = setup_database
+
+    # Create parent at John
+    created_parent = crud.create_model(db, test_models.Parent, parent_1)
+
+    # Update parent from John -> Paul
+    crud.update_model(db, test_models.Parent, created_parent.id, parent_2)
+
+    # Get parent from db
+    db_parent: test_models.Parent = crud.get_model(
+        db, test_models.Parent, created_parent.id
+    )
+
+    assert parent_2["name"] == db_parent.name
+
+
+def test_update_raise_exception_on_invalid_attribute(setup_database):
+    db = setup_database
+
+    # Create parent at John
+    created_parent = crud.create_model(db, test_models.Parent, parent_1)
+
+    with pytest.raises(TypeError) as excinfo:
+        # Update parent with invalid field
+        crud.update_model(db, test_models.Parent, created_parent.id, bad_parent_1)
