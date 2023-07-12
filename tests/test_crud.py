043b3a1b6b625254307130fd96733c3827d82e12
Clayton Black <CBlack@fanatics.com>
2023-07-12 15:22:06 -0400

    feat: Add models for testing and update test_crud.py
    - Added `models_for_test.py` file with `Parent` and `Child` classes.
    - Created a new table called `parents_to_children` to establish a many-to-many relationship between parents and children.
    - Updated the `test_crud.py` file to import the newly added models (`Parent`, `Child`, and `Base`) from `models_for_test.py`.
    - Added test data creation methods (`create_test_data()` and `link_children_to_parents()`) to populate the database with parent and child records.
    - Modified existing tests in the `TestGetModels` class to use the new models (`Parent`) instead of the old model (`TestModel`).

--- tests/test_crud.py
+++ tests/test_crud.py
@@ -3,42 +3,67 @@ import unittest
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import sessionmaker
 
-from sqlalchemy_crud.crud import Base, get_models
+from sqlalchemy_crud.crud import get_models
+from tests.models_for_test import Base, Parent, Child
 
-
-# create models for testing
-class TestModel(Base):
-    __tablename__ = 'test_models'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
+parents = {1: {"name": "parent_test_name_1"}, 2: {"name": "parent_test_name_2"}, 3: {"name": "parent_test_name_3"}}
+bad_parents = {1: {"invalid_field": "That's not what your mom said."}}
+children = {1: {"name": "Mike"}}
 
 
 class TestGetModels(unittest.TestCase):
     def setUp(self):
         # create an in-memory SQLite database for testing
         engine = create_engine('sqlite:///:memory:')
-        Session = sessionmaker(bind=engine)
-        self.db = Session()
+        session = sessionmaker(bind=engine)
+        self.db = session()
 
         Base.metadata.create_all(engine)
 
-        # add some test data to the database
+    def tearDown(self):
+        # remove test da
+        Base.metadata.drop_all(self.db.bind)
+
+    def create_test_data(self):
+
         for i in range(1, 101):
-            test_model = TestModel(id=i, name=f'test_name_{i}')
-            self.db.add(test_model)
+            self.db.add(Parent(name=f"parent_test_name_{i}"))
+
+        for j in range(1, 201):
+            self.db.add(Child(name=f"child_test_name_{j}"))
+
         self.db.commit()
 
+    def link_children_to_parents(self):
+        for i in range(1, 100):
+            parent = self.db.query(Parent).filter(Parent.name == f"parent_test_name_{i}").first()
+
+            # link 2 children to each parent
+            child_1 = self.db.query(Child).filter(Child.name == f"child_test_name_{(2*i)-1}").first()
+            child_2 = self.db.query(Child).filter(Child.name == f"child_test_name_{2*i}").first()
+
+            parent.children.append(child_1)
+            parent.children.append(child_2)
+
+            self.db.commit()
+
     def test_get_models_with_default_offset_and_limit(self):
-        models = get_models(self.db, TestModel)
+
+        self.create_test_data()
+
+        models = get_models(self.db, Parent)
         self.assertEqual(len(models), 100)
-        self.assertEqual(models[0].name, 'test_name_1')
-        self.assertEqual(models[-1].name, 'test_name_100')
+        self.assertEqual(models[0].name, 'parent_test_name_1')
+        self.assertEqual(models[-1].name, 'parent_test_name_100')
 
     def test_get_models_with_custom_offset_and_limit(self):
-        models = get_models(self.db, TestModel, offset=50, limit=10)
+
+        self.create_test_data()
+
+        models = get_models(self.db, Parent, offset=50, limit=10)
         self.assertEqual(len(models), 10)
-        self.assertEqual(models[0].name, 'test_name_51')
-        self.assertEqual(models[-1].name, 'test_name_60')
+        self.assertEqual(models[0].name, 'parent_test_name_51')
+        self.assertEqual(models[-1].name, 'parent_test_name_60')
 
 
 if __name__ == '__main__':
