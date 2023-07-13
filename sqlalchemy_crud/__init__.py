0018be75db02cfd174bdecd9edd3f6b2cd2dfa04
Clayton Black <CBlack@fanatics.com>
2023-07-13 15:55:19 -0400

    Update path to pyproject.toml file
    The code changes update the path to the pyproject.toml file in the sqlalchemy_crud package. The new path is now located in the parent directory of the current file.

--- sqlalchemy_crud/__init__.py
+++ sqlalchemy_crud/__init__.py
@@ -1,7 +1,7 @@
 import tomllib
 from pathlib import Path
 
-toml_path = Path(__file__).parent / "pyproject.toml"
+toml_path = Path(__file__).parent.parent / "pyproject.toml"
 with open(toml_path, "rb") as f:
     pyprojectToml = tomllib.load(f)
 
