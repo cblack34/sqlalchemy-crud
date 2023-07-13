deb866792d4d1201dab008ab20ae5b434edf9060
Clayton Black <CBlack@fanatics.com>
2023-07-13 16:24:02 -0400

    Refactor code to use `toml` library instead of `tomllib`
    - Remove unnecessary import statement for `tomllib`
    - Replace usage of `tomllib.load()` with `toml.load()`
    - Update the import statement for the `toml` library

--- sqlalchemy_crud/__init__.py
+++ sqlalchemy_crud/__init__.py
@@ -1,8 +1,8 @@
-import tomllib
+import toml
 from pathlib import Path
 
 toml_path = Path(__file__).parent.parent / "pyproject.toml"
-with open(toml_path, "rb") as f:
-    pyprojectToml = tomllib.load(f)
+with open(toml_path, "r") as f:
+    pyprojectToml = toml.load(f)
 
 __version__ = pyprojectToml["tool"]["poetry"]["version"]
