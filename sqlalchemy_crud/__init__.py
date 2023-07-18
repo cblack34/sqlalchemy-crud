d3beb9864152fe3a5dfa5cfa8194b65bd2822b62
Clayton Black <CBlack@fanatics.com>
2023-07-17 20:25:41 -0400

    Refactor version retrieval logic
    The code changes remove unnecessary imports and modify the way the version is retrieved from a TOML file.

--- sqlalchemy_crud/__init__.py
+++
@@ -1,8 +0,0 @@
-import toml
-from pathlib import Path
-
-toml_path = Path(__file__).parent.parent / "pyproject.toml"
-with open(toml_path, "r") as f:
-    pyprojectToml = toml.load(f)
-
-__version__ = pyprojectToml["tool"]["poetry"]["version"]
